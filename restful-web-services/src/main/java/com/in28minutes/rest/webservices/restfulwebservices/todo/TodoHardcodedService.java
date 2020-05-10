ccb02302d86322e9ca7e1f625f42189432fd12b1
youne <younesturki@live.fr>
2020-05-10 16:54:05 +0200

    Adding some function in order to send Data to the Front

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoHardcodedService.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoHardcodedService.java
@@ -11,30 +11,37 @@ public class TodoHardcodedService {
 	
 	private static List<Todo> todos = new ArrayList<Todo>();
 	private static int idCounter = 0;
-
+	
 	static {
-		
-		todos.add(new Todo(++idCounter, "in28minutes", "Learn to Play", new Date(), false));
-		todos.add(new Todo(++idCounter, "in28minutes", "Learn about Microservices", new Date(), false));
-		todos.add(new Todo(++idCounter, "in28minutes", "Learn about Angular", new Date(), false));
+		todos.add(new Todo(++idCounter, "in28minutes","Learn to Play", new Date(), false));
+		todos.add(new Todo(++idCounter, "in28minutes","Learn about Microservices 2", new Date(), false));
+		todos.add(new Todo(++idCounter, "in28minutes","Learn about Angular", new Date(), false));
 	}
 	
-	public List<Todo> findAll(){
+	public List<Todo> findAll() {
 		return todos;
+	}
+	
+	public Todo saveTodo(Todo todo) {
+		if(todo.getId() == -1 || todo.getId() == 0) {
+			todo.setId(++idCounter);
+			todos.add(todo);
+		} else {
+			deleteById(todo.getId());
+			todos.add(todo);
+		}
+		return todo;
 	}
 	
 	public Todo deleteById(long id) {
 		Todo todo = findById(id);
-		if(todo == null) return null;
-		if(todos.remove(todo)) {
-			return todo;	
-		}
-		return null;
+		todos.remove(todo);
+		return todo;
 	}
-
+	
 	public Todo findById(long id) {
-		for(Todo todo : todos) {
-			if (todo.getId() == id) {
+		for (Todo todo : todos) {
+			if(todo.getId() == id) {
 				return todo;
 			}
 		}
