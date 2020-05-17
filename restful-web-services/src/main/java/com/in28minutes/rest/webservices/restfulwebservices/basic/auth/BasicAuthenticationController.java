a1fda742e427e9b6ff36a0b99ad0b3a221954d79
youne <younesturki@live.fr>
2020-05-17 15:59:32 +0200

    Adding two files to authenticate with the front

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/auth/BasicAuthenticationController.java
@@ -0,0 +1,19 @@
+package com.in28minutes.rest.webservices.restfulwebservices.basic.auth;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins="http://localhost:4200")
+@RestController
+public class BasicAuthenticationController {
+	
+	
+	@GetMapping("/basicauth")
+	public AuthenticationBean authenticationBean() {
+		//throw new RuntimeException("Some Error has Happened! Contact Support at ***-***");
+		return new AuthenticationBean("You are authenticated");
+	}
+	
+}
